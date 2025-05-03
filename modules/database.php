3527efafcbbf9cd35adab25e0ec94b68d2c03343
Aakash Dhakal <anamoldhakal22@gmail.com>
2025-05-03 16:59:51 +0545

    Remove merge conflict markers and clean up database and OTP configuration files; streamline main home page structure.

--- modules/database.php
+++ modules/database.php
@@ -9,17 +9,6 @@ $username = $_ENV['DB_USERNAME'];
 $password = $_ENV['DB_PASSWORD'];
 $database = $_ENV['DB_NAME'];
 
-
-<<<<<<< HEAD
-//connect to the database
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "sangeet";
-=======
->>>>>>> 532633b780780f32aaacb13c66bf24a0cda4baee
-$mysqli = new mysqli($servername, $username, $password, $database);
-
 // Check connection
 if ($mysqli->connect_error) {
     die("Connection failed: {$mysqli->connect_error}");
