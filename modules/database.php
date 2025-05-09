50effc109c2de7f7421750679129c0d900d6645a
Aakash Dhakal <anamoldhakal22@gmail.com>
2025-05-09 23:45:38 +0545

    Update database variable name in database.php and remove debug echo in trendingSongs.php

--- modules/database.php
+++ modules/database.php
@@ -7,11 +7,10 @@
 $servername = getenv('DB_SERVER');
 $username = getenv('DB_USERNAME');
 $password = getenv('DB_PASSWORD');
-$database = getenv('DB_DATABASE');
+$database = getenv('DB_NAME');
 $port = getenv('DB_PORT');
 
 $mysqli = new mysqli($servername, $username, $password, $database, $port);
-echo $servername . " $username $password $database $port and fuck you";
 // Check connection
 if ($mysqli->connect_error) {
     die("Connection failed: {$mysqli->connect_error}");
